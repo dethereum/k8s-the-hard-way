e472de3e73ffbcc51a5d146d084d4b57d348122e
haiturtle <haiturtle@haiturtle.com>
2024-02-25 15:22:35 -0500

    INIT: terraform

---
+++ main.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "4.51.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = file("creds.json")
+
+  project = var.project_id
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
